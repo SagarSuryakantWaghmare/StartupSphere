27f431ed2f7370cc6635ee56015d519eb641e23c
Sagar <sagarwaghmare1384@gmail.com>
2025-02-24 22:33:29 +0530

    Logo added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,15 +4,14 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <>
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-         <nav className="flex justify-between items-center">
-        <Link href="/">
-          <Image src="/logo.png" alt="logo" width={50} height={50} />
-        </Link>
-         </nav>
-    </header>
+      <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+        <nav className="flex justify-between items-center">
+          <Link href="/">
+            <Image src="/logo.png" alt="logo" width={200} height={100} />
+          </Link>
+        </nav>
+      </header>
     </>
-   
   );
 };
 
