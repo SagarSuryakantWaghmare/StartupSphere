a608e43611b2d719a81371893f09963188de7de9
Sagar <sagarwaghmare1384@gmail.com>
2025-02-26 19:09:18 +0530

    Updating the global.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,54 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local"; // Import localFont correctly
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 
-const workSans=localFont(options:{
-  src:[
+const workSans = localFont({
+  src: [
     {
-      path:'./fonts/WorkSans-Black.ttf',
-      weight:'900',
-      style:'normal'
+      path: "./fonts/WorkSans-Black.ttf",
+      weight: "900",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-ExtraBold.ttf',
-      weight:'800',
-      style:'normal'
+      path: "./fonts/WorkSans-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-Bold.ttf',
-      weight:'700',
-      style:'normal'
+      path: "./fonts/WorkSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-SemiBold.ttf',
-      weight:'600',
-      style:'normal'
+      path: "./fonts/WorkSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-Medium.ttf',
-      weight:'500',
-      style:'normal'
+      path: "./fonts/WorkSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-Regular.ttf',
-      weight:'400',
-      style:'normal'
+      path: "./fonts/WorkSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-Thin.ttf',
-      weight:'200',
-      style:'normal'
+      path: "./fonts/WorkSans-Thin.ttf",
+      weight: "100", // Corrected weight
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-ExtraLight.ttf',
-      weight:'100',
-      style:'normal'
+      path: "./fonts/WorkSans-ExtraLight.ttf",
+      weight: "200", // Corrected weight
+      style: "normal",
     },
-
   ],
-  variable:'--font-work-sans',
-})
+  variable: "--font-work-sans",
+});
 
 export const metadata: Metadata = {
   title: "InvoWave",
@@ -62,9 +62,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={workSans.variable}
-      >
+      <body className={workSans.variable}>
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
