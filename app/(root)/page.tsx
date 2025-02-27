a753430a1c0bc4902a2d5f0ca4286352f61981d8
Sagar <sagarwaghmare1384@gmail.com>
2025-02-27 22:40:07 +0530

    Most of the work done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,52 +1,56 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard,{StartupTypeCard} from "@/components/StartupCard";
-import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
-export default async function Home({ searchParams }: { searchParams: { query?: string } }) {
-  const query = searchParams?.query || "";
-  
-  // Fetch posts from Sanity
-  let posts = [];
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const params = { search: query || null };
 
-  try {
-    // Correct fetch syntax using client.fetch or sanityFetch
-    const response = await sanityFetch({ query: STARTUPS_QUERY });
-    posts = response?.data || [];
-  } catch (error) {
-    console.error("Error fetching startups:", error);
-  }
+  const session = await auth();
+
+  console.log(session?.id);
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
-      {/* Main Landing Page */}
       <section className="purple_container">
         <h1 className="heading">
-          Pitch Your Startup,<br />Connect with <br />Entrepreneurs
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
         </h1>
+
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitches, and get Noticed in Virtual Competitions.
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
         </p>
+
         <SearchForm query={query} />
       </section>
 
-      {/* Second Section - Display Startups */}
       <section className="section_container">
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
+
         <ul className="mt-7 card_grid">
-          {posts.length > 0 ? (
+          {posts?.length > 0 ? (
             posts.map((post: StartupTypeCard) => (
-              <StartupCard key={post._id} post={post} />
+              <StartupCard key={post?._id} post={post} />
             ))
           ) : (
-            <p className="no-results">No Startups found</p>
+            <p className="no-results">No startups found</p>
           )}
         </ul>
       </section>
-      <SanityLive />
 
+      <SanityLive />
     </>
   );
 }
