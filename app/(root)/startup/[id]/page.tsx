afe9c8a36c03c7c94235b7420234f7b0e8d76426
Sagar <sagarwaghmare1384@gmail.com>
2025-03-04 20:13:32 +0530

    Modified the startup card page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,13 +1,83 @@
+import { client } from "@/sanity/lib/client";
+import { notFound } from "next/navigation";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { sanityFetch } from "@/sanity/lib/live";
+import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { formatDate } from "@/lib/utils";
+import { Suspense } from "react";
+import markdownit from "markdown-it";
+import { Skeleton } from "@/components/ui/skeleton";
+const md=markdownit();
+export const experimental_ppr = true;
 
-import React from 'react'
+const page = async ({ params }: { params: { id: string } }) => {
+  const id = (await params).id;
 
-const page = async ({params}:{params:{id:string}}) => {
-    const id = (await params).id;
+  const { data: post } = await sanityFetch({
+    query: STARTUP_BY_ID_QUERY,
+    params,
+  });
+  if (!post) return notFound();
+  const parsedContent=md.render(post?.pitch||" ");
   return (
     <>
-    <h1 className='text-3xl '>This is a startup number:{id}</h1>
-    </>
-  )
-}
+      <section className="purple_container !min-h-[230px]">
+        <p className="tag">{formatDate(post?._createdAt)}</p>
+        <h1 className="heading">{post.title}</h1>
+        <p className="sub-heading !max-=w-5xl">{post.description}</p>
+      </section>
+      <section className="section_container">
+        <img
+          src={post.image}
+          alt="thumbnail"
+          className="w-full h-auto rounded-xl"
+        />
+        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
+          <div className="flex-between gap-5">
+            <Link
+              href={`/users/${post.author?.id}`}
+              className="flex gap-2 items-center mb-3"
+            >
+              <Image
+                src={post.author.image}
+                alt="avathar"
+                width={64}
+                height={64}
+                className="rounded-full drop-shadow-lg"
+              ></Image>
+              <div>
+                <p className="text-20-medium">{post.author.name}</p>
+                <p className="text-16-medium ">{post.author.username}</p>
+              </div>
+            </Link>
 
-export default page
+            <p className="category-tag">{post.category}</p>
+          </div>
+          <h3 className="category-tag">
+           Pitch Details
+          </h3>
+          {parsedContent?(
+            <article
+            className="prose max-w-4xl font-work-sans break-all"
+            dangerouslySetInnerHTML={{__html: parsedContent}}
+            />
+          ):
+          (
+            <p className="no-result">
+               No Details provided
+            </p>
+          )}
+        </div>
+        <hr className="divider" />
+        {/* TODO:Add the recommended feature here */}
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+         
+        </Suspense>
+      </section>
+    </>
+  );
+};
+
+export default page;
